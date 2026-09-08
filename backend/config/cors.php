1d25bf9301d6d27fc8c9f4033a492816e88adbd6
rgngrc <ayesreginagrace0808@gmail.com>
2026-09-09 05:45:49 +0800

    fix: allow production domain in CORS config

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'https://skmamatid-eap.duckdns.org',
+    ],
 
     'allowed_origins_patterns' => [],
 
